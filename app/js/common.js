45ccff01536de45e8329ccca6fbfac14329e20ef
unknown <petromath77>
2017-05-16 17:41:36 +0400

    mmenu work

--- app/js/common.js
+++ app/js/common.js
@@ -2,7 +2,20 @@ $(function() {
 
 	// Custom JS
 	$('#my-menu').mmenu({
-
+		extensions: ['theme-dark', 'fx-listitems-slide', 'pagedim-black'],
+		navbar: {
+			title: '<img src="img/logo.png" alt="logo">'
+		},
+		offCanvas: {
+               position  : "right"
+            }
       });
-
+	var api = $('#my-menu').data('mmenu');
+	api.bind('open:finish', function(){
+		$('.hamburger').addClass('is-active');
+		$('a.logo').css('opacity', '0');
+	}).bind('close:finish', function(){
+		$('.hamburger').removeClass('is-active');
+		$('a.logo').css('opacity', '1');
+	});
 });
--- gulpfile.js
+++ gulpfile.js
@@ -27,7 +27,7 @@ gulp.task('common-js', function() {
 gulp.task('js', ['common-js'], function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
-		'app/libs/jquery-mmenu/source/jquery.mmenu.min.all.js',
+		'app/libs/jquery-mmenu/jquery.mmenu.all.min.js',
 		'app/js/common.min.js', // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))
