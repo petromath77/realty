23e5951d5199a1bcc67a451d93d2d95d110f39b8
petromath77 <petromath77@gmail.com>
2017-05-23 00:38:22 +0300

    hover-best-apart

--- gulpfile.js
+++ gulpfile.js
@@ -28,6 +28,7 @@ gulp.task('js', ['common-js'], function() {
 	return gulp.src([
 		'app/libs/jquery/dist/jquery.min.js',
 		'app/libs/jquery-mmenu/jquery.mmenu.all.min.js',
+		'app/libs/magnific-popup/jquery.magnific-popup.min.js',
 		'app/js/common.min.js', // Всегда в конце
 		])
 	.pipe(concat('scripts.min.js'))
